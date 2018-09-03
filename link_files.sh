618da62c9f9ee058d2d9066b3f877c8557e1444d
Yuya Tsuda <yytsuda@gmail.com>
2018-09-03 23:52:21 +0900

    Improve link_files.sh

--- link_files.sh
+++ link_files.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-# bash -x or set -x
-# 実行されたコマンドの表示
+# None of dst files should exist.
+# (You should move these files before running this script.)
+# If exists, print these files, and exit without doing anything.
 
-# for file in files; do
-#     # ln -s [src] [dst]
-#     ln -s "$(pwd)/$file" "$HOME/$file"
-# done
+any_file_already_exists=false
 
-# files.txt の各行のファイルを $file に読む
 while read file; do
-    set -x
-    ln -s "$(pwd)/$file" "$HOME/$file"
-    set +x
+    dst="$HOME/$file"
+    # if file exists in dst position:
+    if [ -f "$dst" ] ; then
+        # if that file is the first one:
+        if [ "$any_file_already_exists" = false ]; then
+            echo "The following files already exist:"
+            any_file_already_exists=true
+        fi
+        echo "$dst"
+    fi
+done < files.txt
+
+if [ "$any_file_already_exists" = true ] ; then
+    echo "Link files: failed."
+    exit 1
+fi
+
+# Link files:
+echo "Link files: start."
+
+# read each line in files.txt to $file
+while read file; do
+    src="$(pwd)/$file"
+    dst="$HOME/$file"
+    set -x                      # enable printing commands
+    ln -s $src $dst
+    set +x                      # disable printing
 done < files.txt
