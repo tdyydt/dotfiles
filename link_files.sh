d1fa4d12e543f4eba74f66f956f6361a1577930c
Yuya Tsuda <yytsuda@gmail.com>
2018-09-04 00:03:59 +0900

    Improve script again

--- link_files.sh
+++ link_files.sh
@@ -1,37 +1,17 @@
 #!/bin/bash
 
-# None of dst files should exist.
-# (You should move these files before running this script.)
-# If exists, print these files, and exit without doing anything.
-
-any_file_already_exists=false
-
-while read file; do
-    dst="$HOME/$file"
-    # if file exists in dst position:
-    if [ -f "$dst" ] ; then
-        # if that file is the first one:
-        if [ "$any_file_already_exists" = false ]; then
-            echo "The following files already exist:"
-            any_file_already_exists=true
-        fi
-        echo "$dst"
-    fi
-done < files.txt
-
-if [ "$any_file_already_exists" = true ] ; then
-    echo "Link files: failed."
-    exit 1
-fi
-
 # Link files:
-echo "Link files: start."
 
 # read each line in files.txt to $file
 while read file; do
     src="$(pwd)/$file"
     dst="$HOME/$file"
-    set -x                      # enable printing commands
-    ln -s $src $dst
-    set +x                      # disable printing
+    # if file exists in dst position, ignore it.
+    if [ -f "$dst" ] ; then
+        echo "Ignored: $dst already exists."
+    else
+        set -x                  # enable printing commands
+        ln -s $src $dst
+        set +x                  # disable printing
+    fi
 done < files.txt
