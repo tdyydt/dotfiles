d7e54c14f47b9d821627b9482ecf71bff073131b
Yuya Tsuda <yytsuda@gmail.com>
2018-09-04 00:54:22 +0900

    Update link_files

--- link_files.sh
+++ link_files.sh
@@ -10,8 +10,8 @@ while read file; do
     if [ -f "$dst" ] ; then
         echo "Ignored: $dst already exists."
     else
-        set -x                  # enable printing commands
-        ln -s $src $dst
-        set +x                  # disable printing
+        # `set -x` to display executed commands
+        # via: https://stackoverflow.com/questions/13195655/bash-set-x-without-it-being-printed
+        (set -x ; ln -s $src $dst)
     fi
 done < files.txt
